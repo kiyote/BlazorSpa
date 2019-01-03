d6231ddda68fceac0a3b35dff31f8d70be9d7cde
Todd Lang <Todd.Lang@D2L.com>
2019-01-03 15:26:23 -0500

    Fixing the profile link to highlight when selected

--- src/BlazorSpa.Client/Layouts/NavBar.cshtml.cs
+++ src/BlazorSpa.Client/Layouts/NavBar.cshtml.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using BlazorSpa.Client.Components;
 using BlazorSpa.Client.Pages.Auth;
+using Microsoft.AspNetCore.Blazor;
 using Microsoft.AspNetCore.Blazor.Components;
 
 namespace BlazorSpa.Client.Layouts {
@@ -17,11 +19,20 @@ namespace BlazorSpa.Client.Layouts {
 
 		protected NavBarItem NavItemLogIn { get; set; }
 
+		protected NavBarItem NavItemProfile { get; set; }
+
 		public string AuthUrl {
 			get {
 				return $"{_config.AuthUrl}&redirect_uri={_config.Host}{ValidateComponent.Url}";
 			}
 		}
 
+		public void OnPointerEnter( UIPointerEventArgs e ) {
+			Debug.WriteLine( "Enter" );
+		}
+
+		public void OnPointerLeave( UIPointerEventArgs e ) {
+			Debug.WriteLine( "Leave" );
+		}
 	}
 }
