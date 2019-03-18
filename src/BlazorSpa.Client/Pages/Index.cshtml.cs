48c36af4bfa6cba33c37f0478b42e981c892963b
Todd Lang <Todd.Lang@D2L.com>
2019-03-18 16:52:29 -0400

    Adding Index page code-behind

---
+++ src/BlazorSpa.Client/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorSpa.Client.Pages {
+	public class IndexComponent: ComponentBase {
+		public static string Url = "/";
+
+	}
+}
