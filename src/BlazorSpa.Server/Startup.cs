5c3ec0a58858d4009501aa085b7c2af9b583ef22
Todd Lang <Todd.Lang@D2L.com>
2019-03-21 11:17:38 -0400

    Applying license -Fixing nesting by setting it to not root

--- src/BlazorSpa.Server/Startup.cs
+++ src/BlazorSpa.Server/Startup.cs
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018-2019 Todd Lang
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 using BlazorSpa.Repository;
 using BlazorSpa.Repository.Cognito;
 using BlazorSpa.Repository.DynamoDb;
@@ -23,149 +38,137 @@ using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 
-namespace BlazorSpa.Server
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+namespace BlazorSpa.Server {
+	public class Startup {
+		public Startup( IConfiguration configuration ) {
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddLogging(builder => builder
-               .SetMinimumLevel(LogLevel.Debug)
-            );
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices( IServiceCollection services ) {
+			services.AddLogging( builder => builder
+				.SetMinimumLevel( LogLevel.Debug )
+			);
 
-            services
-                .AddConnections()
-                .AddSignalR(o => o.KeepAliveInterval = TimeSpan.FromSeconds(5))
-                .AddNewtonsoftJsonProtocol();
+			services
+				.AddConnections()
+				.AddSignalR( o => o.KeepAliveInterval = TimeSpan.FromSeconds( 5 ) )
+				.AddNewtonsoftJsonProtocol();
 
-            services.AddAuthorization(options => {
-                options.AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy => {
-                    policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
-                    policy.RequireClaim(ClaimTypes.NameIdentifier);
-                });
-            });
+			services.AddAuthorization( options => {
+				options.AddPolicy( JwtBearerDefaults.AuthenticationScheme, policy => {
+					policy.AddAuthenticationSchemes( JwtBearerDefaults.AuthenticationScheme );
+					policy.RequireClaim( ClaimTypes.NameIdentifier );
+				} );
+			} );
 
-            services
-                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, SetJwtBearerOptions);
+			services
+				.AddAuthentication( JwtBearerDefaults.AuthenticationScheme )
+				.AddJwtBearer( JwtBearerDefaults.AuthenticationScheme, SetJwtBearerOptions );
 
-            // TODO: Determine if this is still needed for SignalR
-            services.AddCors(options => options.AddPolicy("CorsPolicy",
-                builder => {
-                    builder.AllowAnyMethod()
-                        .AllowAnyHeader()
-                        .AllowAnyOrigin();
-                }));
+			// TODO: Determine if this is still needed for SignalR
+			services.AddCors( options => options.AddPolicy( "CorsPolicy",
+				 builder => {
+					 builder.AllowAnyMethod()
+						 .AllowAnyHeader()
+						 .AllowAnyOrigin();
+				 } ) );
 
-            services.AddResponseCompression(opts => {
-                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
-                    new[] { "application/octet-stream" });
-            });
+			services.AddResponseCompression( opts => {
+				opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+					new[] { "application/octet-stream" } );
+			} );
 
-            services
-                .AddMvc()
-                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0)
-                .AddNewtonsoftJson(options => {
-                    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
-                    options.SerializerSettings.DateParseHandling = DateParseHandling.None;
-                    options.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
-                });
+			services
+				.AddMvc()
+				.SetCompatibilityVersion( Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0 )
+				.AddNewtonsoftJson( options => {
+					options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+					options.SerializerSettings.DateParseHandling = DateParseHandling.None;
+					options.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+				} );
 
-            services.AddDynamoDb(Configuration.GetSection("DynamoDb").Get<DynamoDbOptions>());
-            services.AddCognito(Configuration.GetSection("Cognito").Get<CognitoOptions>());
-            services.AddS3(Configuration.GetSection("S3").Get<S3Options>());
+			services.AddDynamoDb( Configuration.GetSection( "DynamoDb" ).Get<DynamoDbOptions>() );
+			services.AddCognito( Configuration.GetSection( "Cognito" ).Get<CognitoOptions>() );
+			services.AddS3( Configuration.GetSection( "S3" ).Get<S3Options>() );
 
-            services.AddHttpContextAccessor();
-            services.AddSingleton<IImageRepository, ImageRepository>();
-            services.AddSingleton<IAuthenticationRepository, AuthenticationRepository>();
-            services.AddSingleton<IUserRepository, UserRepository>();
-            services.AddSingleton<IStructureRepository, StructureRepository>();
+			services.AddHttpContextAccessor();
+			services.AddSingleton<IImageRepository, ImageRepository>();
+			services.AddSingleton<IAuthenticationRepository, AuthenticationRepository>();
+			services.AddSingleton<IUserRepository, UserRepository>();
+			services.AddSingleton<IStructureRepository, StructureRepository>();
 
-            services.RegisterServices();
+			services.RegisterServices();
 
-            services.AddSingleton<IContextInformation, ContextInformation>();
-            services.AddSingleton<UserManager>();
-            services.AddSingleton<StructureManager>();
-        }
+			services.AddSingleton<IContextInformation, ContextInformation>();
+			services.AddSingleton<UserManager>();
+			services.AddSingleton<StructureManager>();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseResponseCompression();
-            app.UseAuthentication();
-            app.UseIdentificationMiddleware();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure( IApplicationBuilder app, IWebHostEnvironment env ) {
+			app.UseResponseCompression();
+			app.UseAuthentication();
+			app.UseIdentificationMiddleware();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBlazorDebugging();
-            }
+			if( env.IsDevelopment() ) {
+				app.UseDeveloperExceptionPage();
+				app.UseBlazorDebugging();
+			}
 
-            app.UseCors("CorsPolicy");
-            app.UseSignalR(routes => {
-                routes.MapHub<SignalHub>(SignalHub.Url);
-            });
+			app.UseCors( "CorsPolicy" );
+			app.UseSignalR( routes => {
+				routes.MapHub<SignalHub>( SignalHub.Url );
+			} );
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(name: "default", template: "{controller}/{action}/{id?}");
-            });
+			app.UseMvc( routes => {
+				routes.MapRoute( name: "default", template: "{controller}/{action}/{id?}" );
+			} );
 
-            app.UseBlazor<Client.Startup>();
-        }
+			app.UseBlazor<Client.Startup>();
+		}
 
-        private void SetJwtBearerOptions(JwtBearerOptions options)
-        {
-            var tokenValidationOptions = Configuration.GetSection("TokenValidation").Get<TokenValidationOptions>();
-            var rsa = new RSACryptoServiceProvider();
-            rsa.ImportParameters(
-                new RSAParameters()
-                {
-                    Modulus = Base64UrlEncoder.DecodeBytes(tokenValidationOptions.Modulus),
-                    Exponent = Base64UrlEncoder.DecodeBytes(tokenValidationOptions.Expo)
-                });
-            var key = new RsaSecurityKey(rsa);
+		private void SetJwtBearerOptions( JwtBearerOptions options ) {
+			var tokenValidationOptions = Configuration.GetSection( "TokenValidation" ).Get<TokenValidationOptions>();
+			var rsa = new RSACryptoServiceProvider();
+			rsa.ImportParameters(
+				new RSAParameters() {
+					Modulus = Base64UrlEncoder.DecodeBytes( tokenValidationOptions.Modulus ),
+					Exponent = Base64UrlEncoder.DecodeBytes( tokenValidationOptions.Expo )
+				} );
+			var key = new RsaSecurityKey( rsa );
 
-            options.RequireHttpsMetadata = false;
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = key,
-                ValidIssuer = tokenValidationOptions.Issuer,
-                ValidateIssuer = true,
-                ValidateLifetime = true,
-                ValidateAudience = false,
-                ClockSkew = TimeSpan.FromMinutes(0)
-            };
+			options.RequireHttpsMetadata = false;
+			options.TokenValidationParameters = new TokenValidationParameters {
+				ValidateIssuerSigningKey = true,
+				IssuerSigningKey = key,
+				ValidIssuer = tokenValidationOptions.Issuer,
+				ValidateIssuer = true,
+				ValidateLifetime = true,
+				ValidateAudience = false,
+				ClockSkew = TimeSpan.FromMinutes( 0 )
+			};
 
-            options.Events = new JwtBearerEvents
-            {
-                OnMessageReceived = context => {
-                    var accessToken = context.Request.Query["access_token"];
+			options.Events = new JwtBearerEvents {
+				OnMessageReceived = context => {
+					var accessToken = context.Request.Query["access_token"];
 
-                    // If there is an access token supplied in the url, then
-                    // we check to see if we're actually trying to service
-                    // a SignalR request, and if so, we tuck the token in to
-                    // the context so the request is property authenticated.
-                    if (!string.IsNullOrWhiteSpace(accessToken)
-                        && (context.HttpContext.WebSockets.IsWebSocketRequest
-                            || context.HttpContext.Request.Path.StartsWithSegments(SignalHub.Url)
-                            || context.Request.Headers["Accept"] == "text/event-stream"))
-                    {
-                        context.Token = context.Request.Query["access_token"];
-                    }
-                    return Task.CompletedTask;
-                }
-            };
-        }
-    }
+					// If there is an access token supplied in the url, then
+					// we check to see if we're actually trying to service
+					// a SignalR request, and if so, we tuck the token in to
+					// the context so the request is property authenticated.
+					if( !string.IsNullOrWhiteSpace( accessToken )
+						&& ( context.HttpContext.WebSockets.IsWebSocketRequest
+							|| context.HttpContext.Request.Path.StartsWithSegments( SignalHub.Url )
+							|| context.Request.Headers["Accept"] == "text/event-stream" ) ) {
+						context.Token = context.Request.Query["access_token"];
+					}
+					return Task.CompletedTask;
+				}
+			};
+		}
+	}
 }
